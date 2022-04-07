0badb9def4a8118ba99c461918f39152e2b6147a
Alejandro Vasquez <josejaguer12@gmail.com>
2022-04-06 23:34:12 -0600

    Realizado la parte movil

---
+++ js/main.js
@@ -0,0 +1,9 @@
+//Variables
+const menuBtn = document.querySelector(".header__logo i");
+const headerNav = document.querySelector(".header__nav");
+//Functions
+const toggleNavList = () => {
+  headerNav.classList.toggle("visible");
+};
+//Events
+menuBtn.addEventListener("click", toggleNavList);
