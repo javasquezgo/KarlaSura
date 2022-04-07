df07a0a62c96067b39af54b9e7c69a053b969edf
Alejandro Vasquez <josejaguer12@gmail.com>
2022-04-07 16:44:10 -0600

    finalizo lo tablet

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 //Variables
 const menuBtn = document.querySelector(".header__logo i");
 const headerNav = document.querySelector(".header__nav");
+
 //Functions
 const toggleNavList = () => {
   headerNav.classList.toggle("visible");
 };
+
 //Events
 menuBtn.addEventListener("click", toggleNavList);
